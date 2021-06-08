8e23232f14d3e471ae6c1f65f79959eb17ec7ea8
mtrevin93 <mtrevin93@gmail.com>
2021-06-08 14:30:12 -0400

    added event listener for metals

--- scripts/Metals.js
+++ scripts/Metals.js
@@ -5,6 +5,9 @@ const metals = getMetals()
 document.addEventListener(
     "change",
     (event) => {
+        if (event.target.name === "metal") {
+            window.alert(`User has selected metal number ${event.target.value}`)
+        }
     }
 )
 
