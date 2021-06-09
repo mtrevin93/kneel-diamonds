179aab10d26ee0df9994fc2a06255942d8ee6ec8
mtrevin93 <mtrevin93@gmail.com>
2021-06-09 12:49:28 -0400

    fixed CSS styling for flexbox

--- scripts/KneelDiamonds.js
+++ scripts/KneelDiamonds.js
@@ -28,15 +28,15 @@ export const KneelDiamonds = () => {
         <h1>Kneel Diamonds</h1>
 
         <article class="choices">
-            <section class="choices__metals options">
+            <section class="options__metals options">
                 <h2>Metals</h2>
                 <p>${Metals()}</p>
             </section>
-            <section class="choices__sizes options">
+            <section class="options__sizes options">
                 <h2>Sizes</h2>
                 <p>${DiamondSizes()}</p>
             </section>
-            <section class="choices__styles options">
+            <section class="options__styles options">
                 <h2>Styles</h2>
                 <p>${JewelryStyles()}</p>
             </section>
